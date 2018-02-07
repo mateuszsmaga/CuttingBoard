4ee593ceece2f13cf33cca3a3110bd35f8ccfdee
mateuszsmaga <mateusz.smaga@gmail.com>
2018-02-07 17:56:53 +0100

    More menus, added listeners, first block

--- Assets/Scripts/CollectData.cs
+++ Assets/Scripts/CollectData.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class CollectData : MonoBehaviour {
 
+	private int boardLenght = 40;
+	private int boardThickness = 2;
+	private int numberOfCuts = 1;
+
+	private bool boardChanged = false;
+
+	GameObject board;
+
 	// Use this for initialization
 	void Start () {
-		
+		board = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		board.transform.localScale = new Vector3(50,boardLenght,boardThickness);
+		board.transform.SetParent(transform);
+		board.transform.position = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(boardChanged){
+			board.transform.localScale = new Vector3(1,boardLenght,boardThickness);
+			boardChanged = false;
+		}
 		
+	}
+
+	public void GetAndSetBoardLenght(string newLength){
+		boardLenght = int.Parse(newLength);
+		boardChanged = true;
+	}
+
+	public void GetAndSetNumberOfCuts(string newNumberOfCuts){
+		numberOfCuts = int.Parse(newNumberOfCuts);
+		boardChanged = true;
+	}
+
+	public void GetAndSetBoardThickness(string newThickness){
+		boardThickness = int.Parse(newThickness);
+		boardChanged = true;
 	}
 }
---
+++ Assets/Scripts/UIControlls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControlls : MonoBehaviour {
+
+	private float newLinePosY = 0f;
+	private float addButtonPosY = -30f;
+
+	int linesCount = 0;
+
+	public GameObject addButtonObject;
+
+	public void AddButtonOnClick(GameObject menuLinePrefab){
+		linesCount++;
+		if(linesCount<=12){
+			newLinePosY-=40f;
+			addButtonPosY-=40f;
+			GameObject newMenuLine = Instantiate(menuLinePrefab, new Vector3(0,0,0), Quaternion.identity);
+			//newMenuLine.transform.parent = transform;
+			newMenuLine.transform.SetParent(transform);
+			newMenuLine.transform.localPosition = new Vector3(0, newLinePosY, 0);
+			addButtonObject.transform.localPosition = new Vector3(0, addButtonPosY, 0);
+		}
+
+		if(linesCount==12){
+			Text buttonMessage = addButtonObject.transform.GetChild(0).GetComponent<Text>();
+			buttonMessage.text = "Maks elementów";
+		}
+		
+
+		//menuLinePrefab.transform.parent = transform;
+		//Debug.Log("You have clicked the button!");
+	}
+}
